a14847ddaf4c7086766e6775b808a7f45370ee16
Sanyam Jain <sanyamjain.mca18.du@gmail.com>
2019-01-11 01:32:18 +0530

    Update linked-list.py

--- linked-list.py
+++ linked-list.py
@@ -30,7 +30,7 @@ class Linkedlist:
         '''
         self.head=None
 
-    def insertNode(self,value):
+    def insertNodeBeginning(self,value):
         '''
         objective: To insert node in the beginning
         input parameters:
@@ -43,35 +43,7 @@ class Linkedlist:
         temp.next=self.head #putting the current head in the new nodes next
         self.head=temp  #and now making head pointing to the new element head
 
-    def insertNodeRecursive(self,current,currentNext,value):
-
-        if current==None:
-            self.head=Node(value)
-
-            return
-
-        if current.next==None:
-            if current.data<value:
-                current.next=Node(value)
-            else:
-                temp=current
-                self.head=Node(value)
-                self.head.next=temp
-
-            return
-        
-        
-        if current.data<value and currentNext.data>value:
-            temp=current
-            temp.next=Node(value)
-            temp.next.next=currentNext             
-        
-		else:
-            return self.insertNodeRecursive(current.next,current.next.next,value)
             
-                
-            
-        
     def insertAtEnd(self,temp,value):
         '''
         Objective: To add a node at the end of a linked list
@@ -80,6 +52,7 @@ class Linkedlist:
             value: Value to be inserted
             temp : Current node
         Return Value: None
+
         '''
         #Approach: Recursively
         if temp == None:
@@ -89,7 +62,37 @@ class Linkedlist:
         else:
             return self.insertAtEnd(temp.next,value)
 
+    def insertNodeRecursive(self,current,value):
+        '''
+        objective: To insert a node in the list which should be sorted
+        input parameterd:
+                        self: impllicit parameter
+                        current: it is the current node basis on which we need to add new node
+                        value: new node's value to be inserted
+        '''
+        if current==None: #WHEN NO NODE
+            self.head=Node(value)
+
+        elif current.next==None: #WHEN ONLY ONE NODE
+
+            if current.data<value:
+                current.next=Node(value)
+            else:
+                temp=Node(value)
+                temp.next=self.head
+                self.head=temp   
+        
+        elif current.data<value and current.next.data>value: #WWHEN TO ADD NODE IN B/W TWO NODES
+            
+            temp=Node(value)
+            temp.next=current.next
+            current.next=temp
+
+        else:
+            return self.insertNodeRecursive(current.next,value)
+            
     def deleteNodeIterative(self,value):
+        #ITERATIVE DELETION
         '''
         objective: To delete node from the list iteratively
         input parameters:
@@ -117,6 +120,7 @@ class Linkedlist:
             print(value+' not found')
 
     def deleteNodeRecursive(self,previous,current,value):
+        #RECURSIVE PROCEDURE TO DELETER
         '''
         objective: To delete node from the list recursively
         input parameters:
@@ -127,13 +131,13 @@ class Linkedlist:
         
         '''
         if previous==None:
+            print('empty list')
+
+        elif previous.next==None:
             print('empty')
 
         elif previous.data==value:
             self.head=current
-
-        elif previous.next==None:
-            print('empty')
 
         elif previous.next.data==value:
             previous.next=current.next
@@ -167,44 +171,55 @@ if __name__=='__main__':
     while True:
 
         print("1. INSERT NODE IN THE BEGINNING \n")
-        print("2. DELETE NODE FROM THE BEGINNING \n")
-        print("3. DISPLAY YOUR LIST \n")
-        print("4. INSERT NODE AT THE END \n")
+        print("2. INSERT NODE AT THE END \n")
+        print("3. INSERT NODE RECURSIVELY IN LIST IN SORTED ORDER \n")
+        print("4. DELETE NODE FROM THE LIST\n")
+        print("5. DISPLAY YOUR LIST \n")
+        print("6. ENTER 0 TO EXIT \n")
+        
         print("ENTER YOUR CHOICE :",end=" ")
         ch=int(input())            
 
         if ch==1:
-            print('ENTER NUMBER OF VALUES TO INSERT :',end=" ")
+            print('ENTER NUMBER OF VALUES TO INSERT IN BEGINNING :',end=" ")
             number=int(input())
             for i in range(number): 
                 value=int(input())
-                l.insertNode(value)
+                l.insertNodeBeginning(value)
+
         elif ch==2:
-            print("2.1 ITERATIVELY \n")
-            print("2.2 RECURSIVELY\n")
+            print('ENTER NUMBER OF VALUES TO INSERT AT END :',end=" ")
+            number=int(input())
+            for i in range(number): 
+                value=int(input())
+                l.insertAtEnd(l.head,value)
+
+        elif ch==3:
+            print('ENTER NUMBER OF VALUES TO INSERT IN LIST WHICH WILL BE SORTED :',end=" ")
+            number=int(input())
+            for i in range(number): 
+                value=int(input())
+                l.insertNodeRecursive(l.head,value)
+
+        elif ch==4:
+            print("4.1 ITERATIVELY \n")
+            print("4.2 RECURSIVELY\n")
             ch1=float(input('ENTER CHOICE HOW TO DELETE :'))
 
-            if ch1==2.1:
+            if ch1==4.1:
 
                 print('ENTER THE VALUE TO DELETE :', end=" ")
                 value=int(input())
                 l.deleteNodeIterative(value)
 
-            if ch==2.2:
+            if ch==4.2:
 
                 print('ENTER THE VALUE TO DELETE :', end=" ")
                 value=int(input())
                 l.deleteNodeRecursive(l.head,l.head.next,valuue)
                 
-        elif ch==3:
+        elif ch==5:
             print(l)
 
-        elif ch==4:
-            print('ENTER NUMBER OF VALUES TO INSERT :',end=" ")
-            number=int(input())
-            for i in range(number): 
-                value=int(input())
-                l.insertNodeRecursive(None,l.head.next,value)
-
         else:
-            print("Invalid choice")
+            exit()
